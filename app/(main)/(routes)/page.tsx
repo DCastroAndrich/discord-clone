5a040be4aedf211a0c71ed3ba800863687d3dd89
DCastroAndrich <castroandrich.damian@gmail.com>
2023-10-25 16:30:15 -0300

    Discord clone first commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+
+  )
+}
